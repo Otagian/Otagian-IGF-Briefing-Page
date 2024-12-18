b8f3951cb7e8ec8ed6c68ddc3f65fdf8cdb2fbd2
Otagian <42849411+Otagian@users.noreply.github.com>
2024-12-18 02:13:43 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -108,7 +108,14 @@ export default {
           "frame": "Sagarmatha",
           "mech": "Demigryph"
         },
-       
+        {
+          "callsign": "Siren",
+          "alias": "ALKONOST SIREN/5962A",
+          "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
+          "corpro": "GMS",
+          "frame": "Clipper",
+          "mech": "Dragons Tooth"
+        },       
       ],
       "header": {
         "planet": "Stygia",
