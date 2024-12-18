5491f6c748212df4da4fd8a5c1e2ccb4e74871f4
Otagian <42849411+Otagian@users.noreply.github.com>
2024-12-18 02:17:25 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -125,7 +125,7 @@ export default {
         "ring": "Annamite Line",
         "headerTitle": "HELL'S GATE",
         "headerSubtitle": "Militia Squadron",
-        "subheaderTitle": "Crisis Response",
+        "subheaderTitle": "Strategic Response",
         "subheaderSubtitle": "House Unfuckers",
       },
       "options":{
